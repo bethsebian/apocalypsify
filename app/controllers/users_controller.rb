a8912684a14ef374832b2fb772cfe16a6efa0b7e
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-01-19 08:40:01 -0700

    Refactor controllers to use set_user method
    Refactored some of the controllers which were all setting the
    session's user id the same way, made a method for this and placed it in
    application controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(set_user)
+    @user = User.create(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      set_user
       redirect_to dashboard_path
     else
       flash.now[:error] = {message: @user.errors.full_messages.join(", "), color: "red"}
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
   private
 
-  def set_user
+  def user_params
     params.require(:user).permit(
       :first_name,
       :last_name,
